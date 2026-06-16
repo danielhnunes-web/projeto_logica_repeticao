de62a2a3d62334b133093e4e8bc021e878475497
Daniel <daniel.h.nunes@aluno.senai.br>
2026-06-16 16:43:25 -0300

    criei outras três const 'inputNum, btnNum, divContAcum'

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -7,3 +7,7 @@ for(i = 0;i < 10; i++){
     divFor.innerHTML += `${i} - amor <br>>`
 
 }
+
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
