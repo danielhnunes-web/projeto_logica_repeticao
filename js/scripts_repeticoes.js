482701201fe95397355f93a751d078de56080d46
Daniel <daniel.h.nunes@aluno.senai.br>
2026-06-16 16:54:26 -0300

    criado o cod de exemplo da estrutura de repetição

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -11,3 +11,18 @@ for(i = 0;i < 10; i++){
 const inputNum = document.querySelector('#num')
 const btnNum = document.querySelector('#btn-num')
 const divContAcum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click', (evt) =>{
+    let numDigitado = Number(inputNum.value)
+
+    cont++
+    acum += numDigitado
+
+    divContAcum.innerHTML = `Total de Número Digitado é: ${cont} <br>
+    A soma dos números é: ${acum}`
+
+    inputNum.value = ''
+})
