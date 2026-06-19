14cc4abae7b0afec99f797df7829034f7ece39bf
Daniel <daniel.h.nunes@aluno.senai.br>
2026-06-19 16:29:53 -0300

    exportei o calculoPercentual

---
+++ js/scripts_calculos.js
@@ -0,0 +1,15 @@
+const calculoPercentual = (valor) => {
+    let valorCalculado = 0.0 
+
+    if (valor <= 100){
+        valorCalculado = valor * 0.10
+    }else if( valor <= 1000 ){
+        valorCalculado = valor * 0.15
+    }else{
+        valorCalculado = valor * 0.20
+    }
+
+    return valorCalculado
+}
+
+export{calculoPercentual}
